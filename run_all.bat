a438ebb332a70035429b43457183f348de485075
erenzmk <erengencer1704@gmail.com>
2025-08-06 02:41:07 +0200

    run_all.bat automatisiert

--- run_all.bat
+++ run_all.bat
@@ -1,20 +1,6 @@
 @echo off
-setlocal
-
-set "DEFAULT_MONTH=data\reports\2025-07"
-set /p "MONTH=Monatsordner [%DEFAULT_MONTH%]: "
-if "%MONTH%"=="" set "MONTH=%DEFAULT_MONTH%"
-
-set "DEFAULT_LIST=Liste.xlsx"
-set /p "LIST=Pfad zur Liste [%DEFAULT_LIST%]: "
-if "%LIST%"=="" set "LIST=%DEFAULT_LIST%"
-
-set "DEFAULT_OUT=report.csv"
-set /p "OUTPUT=Ausgabedatei [%DEFAULT_OUT%]: "
-if "%OUTPUT%"=="" set "OUTPUT=%DEFAULT_OUT%"
-
+set "MONTH=data\reports\2025-07"
+set "LIST=Liste.xlsx"
+set "OUTPUT=report.csv"
 python -m dispatch.main run-all "%MONTH%" "%LIST%" --output "%OUTPUT%"
-
 echo %DATE% %TIME% - run_all.bat ausgeführt mit "%MONTH%" "%LIST%" "%OUTPUT%" >> "logs\arbeitsprotokoll.md"
-
-endlocal
